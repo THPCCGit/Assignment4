e701b54d1fa7c77c3a6c76469b01eac696300368
THPCCGit <30302656+THPCCGit@users.noreply.github.com>
2017-07-19 17:58:37 -0700

    Patch 1.00 Changes
    1. Changed README.txt to display correct information
    2. main.java now displays "Hello World!"

--- main.java
+++ main.java
@@ -1,33 +1,18 @@
-
 /**
  * Write a description of class main here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Tyde Hashimoto
+ * @version 2017.19.07
  */
 public class main
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
     /**
      * Constructor for objects of class main
      */
     public main()
     {
-        // initialise instance variables
-        x = 0;
+        System.out.print("Hello World!");
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
